0ac324cad73e8f5de330b22a51550d8aa2c1bdda
massaro8 <donatomassaro99@gmail.com>
2024-11-07 15:35:14 +0100

    commit file main

---
+++ main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from handler import router
+
+# Crea un'istanza di FastAPI con specifiche configurazioni per i percorsi di documentazione
+app = FastAPI(
+    # Imposta il percorso personalizzato per il documento OpenAPI JSON
+    # Questo è il documento che descrive formalmente l'intera API.
+    openapi_url="/documentation/json",
+
+    # Specifica un URL personalizzato per la documentazione interattiva di Swagger UI.
+    # Swagger UI permette di esplorare e testare l'API in un'interfaccia grafica.
+    docs_url="/documentation/",
+
+    # Disabilita ReDoc, un'altra interfaccia di documentazione.
+    # Impostando questo valore a None, l'endpoint ReDoc non sarà disponibile.
+    redoc_url=None
+)
+
+# Decoratori per definire una rota
+app.include_router(router)
+
+
+
+
+
+
+
